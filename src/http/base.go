60ed34f6c2a2493ea2dc89387f422e449a280b08
Summer-lights <1067088037@qq.com>
2024-03-25 11:32:28 +0800

    feat(http): JsonBaseResponseCtx

--- src/http/base.go
+++ src/http/base.go
@@ -12,41 +12,37 @@ import (
 	"net/http"
 )
 
-// BaseResponse is the base response struct.
-type BaseResponse[T any] struct {
+// CodeMsgResp is the base response struct.
+type CodeMsgResp struct {
 	// Code represents the business code, not the http status code.
 	Code int `json:"code" xml:"code"`
 	// Msg represents the business message, if Code = BusinessCodeOK,
 	// and Msg is empty, then the Msg will be set to BusinessMsgOk.
 	Msg string `json:"msg" xml:"msg"`
-	// Data represents the business data.
-	Data T `json:"data,omitempty" xml:"data,omitempty"`
 }
 
 // JsonBaseResponseCtx writes v into w with http.StatusOK.
 func JsonBaseResponseCtx(ctx context.Context, w http.ResponseWriter, v any) {
-	code, v := wrapBaseResponse(v)
-	httpx.WriteJsonCtx(ctx, w, code, v)
+	if err, ok := v.(error); ok {
+		code, resp := wrapError(err)
+		httpx.WriteJsonCtx(ctx, w, code, resp)
+	} else {
+		httpx.WriteJsonCtx(ctx, w, http.StatusOK, v)
+	}
 }
 
-func wrapBaseResponse(v any) (int, BaseResponse[any]) {
-	var resp BaseResponse[any]
-	if err, ok := v.(error); ok {
-		var codeMsg *errors2.CodeMsg
-		if errors.As(err, &codeMsg) {
-			resp.Code = codeMsg.Code
-			resp.Msg = codeMsg.Msg
-		} else if grpcStatus, ok := status.FromError(err); ok {
-			resp.Code = int(grpcStatus.Code())
-			resp.Msg = grpcStatus.Message()
-		} else {
-			resp.Code = BusinessCodeError
-			resp.Msg = err.Error()
-		}
+func wrapError(err error) (int, CodeMsgResp) {
+	var resp CodeMsgResp
+	var codeMsg *errors2.CodeMsg
+	if errors.As(err, &codeMsg) {
+		resp.Code = codeMsg.Code
+		resp.Msg = codeMsg.Msg
+	} else if grpcStatus, ok := status.FromError(err); ok {
+		resp.Code = int(grpcStatus.Code())
+		resp.Msg = grpcStatus.Message()
 	} else {
-		resp.Code = BusinessCodeOK
-		resp.Msg = BusinessMsgOk
-		resp.Data = v
+		resp.Code = BusinessCodeError
+		resp.Msg = err.Error()
 	}
 
 	return runtime.HTTPStatusFromCode(codes.Code(resp.Code)), resp
